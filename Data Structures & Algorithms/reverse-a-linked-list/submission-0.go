359939075475b9506e48e47290c6f5f8d355af95
Nikola Sljivkov <44641906+sljivkov@users.noreply.github.com>
2026-08-17 22:26:27 +0200

    Add: reverse-a-linked-list - submission-0

---
+++ Data Structures & Algorithms/reverse-a-linked-list/submission-0.go
@@ -0,0 +1,13 @@
+func reverseList(head *ListNode) *ListNode {
+	curr := head
+	var prev *ListNode
+	for curr != nil {
+		next := curr.Next
+		curr.Next = prev
+		prev = curr
+		curr = next
+
+	}
+
+	return prev
+}
